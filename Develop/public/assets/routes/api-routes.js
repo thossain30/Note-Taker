e19c0224f20338795ce87995cf64fcfdf1dadaee
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-04-11 19:01:44 -0400

    changed paths for routes

---
+++ Develop/public/assets/routes/api-routes.js
@@ -0,0 +1,26 @@
+const notes = require("express").Router();
+const fs = require("fs");
+const path = require("path");
+
+const { v4: uuidv4 } = require('uuid');
+
+notes.get("api/notes", (req, res) => {
+    let data = fs.readFileSync(path.join(__dirname, '../db/db.json'));
+    res.send(JSON.parse(data));
+})
+
+notes.post("api/notes", (req, res) => {
+    let data = fs.readFileSync(path.join(__dirname, '../db/db.json'));
+    let curnotes = JSON.parse(data);
+    const {title, text} = req.body;
+    const newNote = {
+        title,
+        text,
+        id:uuidv4()
+    };
+    curnotes.push(newNote);
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'));
+    res.send(newNote);
+});
+
+module.exports = notes;
