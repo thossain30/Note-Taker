eb61a12dc345ab2acb435652176a80989a99c74d
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-04-11 19:29:57 -0400

    changed paths again

--- public/assets/routes/api-routes.js
+++ public/assets/routes/api-routes.js
@@ -5,12 +5,12 @@ const path = require("path");
 const { v4: uuidv4 } = require('uuid');
 
 notes.get("api/notes", (req, res) => {
-    let data = fs.readFileSync(path.join(__dirname, '../../db/db.json'));
+    let data = fs.readFileSync(path.join(__dirname, '../db/db.json'));
     res.send(JSON.parse(data));
 })
 
 notes.post("api/notes", (req, res) => {
-    let data = fs.readFileSync(path.join(__dirname, '../../db/db.json'));
+    let data = fs.readFileSync(path.join(__dirname, '../db/db.json'));
     let curnotes = JSON.parse(data);
     const {title, text} = req.body;
     const newNote = {
@@ -19,6 +19,8 @@ notes.post("api/notes", (req, res) => {
         id:uuidv4()
     };
     curnotes.push(newNote);
-    fs.writeFileSync(path.join(__dirname, '../../db/db.json'));
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'));
     res.send(newNote);
 });
+
+module.exports = notes;
