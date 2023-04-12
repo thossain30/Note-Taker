2c339dca2f37b24250615c312df89dce189a6b3b
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-04-11 21:11:21 -0400

    fixed post

--- public/assets/routes/api-routes.js
+++ public/assets/routes/api-routes.js
@@ -8,23 +8,23 @@ notes.get("/notes", (req, res) => {
     let data = fs.readFileSync(path.join(__dirname, '../../../db/db.json'));
     res.send(JSON.parse(data));
 })
-// db\db.json
-// public\assets\routes\api-routes.js
 
 notes.post("/notes", (req, res) => {
     let data = fs.readFileSync(path.join(__dirname, '../../../db/db.json'));
     let curnotes = JSON.parse(data);
-    console.log(curnotes);
     const {title, text} = req.body;
+
+    console.log("request: ", req.body);
     const newNote = {
         title,
         text,
         id:uuidv4()
     };
+    console.log("newnote: ", newNote);
     curnotes.push(newNote);
     // writing new note to db.json
-    fs.writeFileSync(path.join(__dirname, '../../../db/db.json'));
-    res.send(newNote);
+    fs.writeFileSync(path.join(__dirname, '../../../db/db.json'), JSON.stringify(curnotes));
+    res.send(curnotes);
     res.send('Note sent!');
 });
 
