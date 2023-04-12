83733976e130b3d833ab75580b606fca63ee08d8
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-04-11 21:24:51 -0400

    changed html routes

--- public/assets/routes/html-routes.js
+++ public/assets/routes/html-routes.js
@@ -1,11 +1,12 @@
 const path = require("path");
+const app = require("express").Router();
 
-module.exports = (app) => {
-    app.get("/notes", (req,res) => {
-        res.sendFile(path.join(__dirname, "../notes.html"));
-    });
+app.get("/notes", (req,res) => {
+    res.sendFile(path.join(__dirname, "../notes.html"));
+});
 
-    app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname, "../index.html"));
-    });
-}
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+module.exports = app;
